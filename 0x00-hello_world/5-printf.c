07ebb6b669f242ba34701d5653b649eec4417ea3
FrimAngelo <addoangel26@gmail.com>
2023-04-21 04:23:57 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main -A program that prints a line using thr printf function
- * Return: 0 (sccess)
+ * main - A program that prints a line sing printf function
+ * Return: 0 (Success)
  */
 int main(void)
 {
