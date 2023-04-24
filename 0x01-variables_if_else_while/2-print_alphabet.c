4a7b851b66cb52fd576a995f762d98d46933c2fe
FrimAngelo <addoangel26@gmail.com>
2023-04-24 04:48:01 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,10 @@
 int main(void)
 {
 	char letter;
-	fot (letter = 'a'; letter <= 'z'; letter++)
+	for
+		((letter = 'a');
+			letter <= 'z';
+			letter++);
 		putchar(letter);
 	putchar('\n');
 	return (0);
