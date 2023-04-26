c78deb8fef0f83e90df3b79b2fc4dc6ae2f5641c
FrimAngelo <addoangel26@gmail.com>
2023-04-26 14:26:54 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,23 +4,18 @@
  *  @n: integer to be tested
  *  Return: abs(n)
  */
-int _abs(int n)
-{
-	if (n >= 0)
-		return (n);
-	else
-		return (-1 * n);
-}
-/**
- * print_last_digit - print the last digit of a number
- * @n: integer who last digit is to be printed
- * Return: last digit of n
- */
 int print_last_digit(int n)
 {
-	int lastDigit;
+	int last;
 
-	lastDigit = _abs((n % 10));
-	_putchar(lastDigit + 48);
-	return (lastDigit);
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last =  last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
 }
