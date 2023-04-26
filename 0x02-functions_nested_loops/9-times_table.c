e91362f49896b132b5ea3d9b143404696ce4a320
FrimAngelo <addoangel26@gmail.com>
2023-04-26 15:09:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,28 +4,33 @@
  */
 void times_table(void)
 {
-	int i, j, prod;
+	int x, y, z, u, d;
 
-	for  (i = 0; i < 10; i++)
+	for (x - 0; x <= 9; x++)
 	{
-
-		for (j = 0; j < 10; j++)
+		for (y = 0; y <= 9; y++)
 		{
-			if (j == 0)
+			z = x * y;
+
+			if (z > 9)
 			{
-				_putchar(48);
-				continue;
-			}
-			prod = i * j;
-			if (prod >= 10)
-			{
-				_putchar(prod / 10 + 48);
-				_putchar(prod % 10 + 48);
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				_putchar('');
-				_putchar(prod + 48);
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
 			}
 		}
 		_putchar('\n');
