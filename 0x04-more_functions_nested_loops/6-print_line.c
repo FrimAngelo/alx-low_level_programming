660b8cb40256bad2fc3f4d1aa84cb80bc0b81303
FrimAngelo <addoangel26@gmail.com>
2023-04-28 16:42:20 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - it takes one argument
+ * @n: lenth of line
+ *
+ * Return:return void
+ */
+void print_line(int n)
+{
+	int num;
+
+	for (num = 0; num < n; num++)
+		_putchar('_');
+	}
+	_putchar('\n');
+}
