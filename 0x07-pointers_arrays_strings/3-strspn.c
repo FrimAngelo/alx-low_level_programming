8d38c29cc4fe2768d5a558b914e0ae60476dfa6d
FrimAngelo <addoangel26@gmail.com>
2023-05-08 10:03:03 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,24 +11,22 @@ unsigned int _strspn(char *s, char *accept)
 int i = 0, j;
 int matches = 0;
 
-while (s[i] != '\0')
-	/ iterate through string /
+while (s[i] != '\0'); / iterate through string /
 {
 
 for (j = 0; accept[j] !=
 		'\0'; j++) / iterate through target /
 {
-if (s[i] == accept[j]) /
-	record & break at first match /
+if (s[i] == accept[j]) /record &
+	break at first match /
 {
 matches++;
 break;
 }
 if (accept[j + 1] == '\0'
 		&& s[i] != accept[j])
-return (matches);
-
-/ return if idx doesn't match /
+return (matches);/
+return if idx does not match /
 }
 i++;
 }
